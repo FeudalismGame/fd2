db5c463041ff8e65e63b3e3f57102df19e4c5fb9
FeudalismGame <96882000+FeudalismGame@users.noreply.github.com>
2022-01-12 01:26:30 +0300

    Add files via upload

--- web3connection.js
+++ web3connection.js
@@ -107,7 +107,7 @@ async function fetchAccountData() {
   // Go through all accounts and get their $Wheat balance
   const rowResolvers = accounts.map(async (address) => {
     const balance = await new contractProvider.eth.Contract(wheatABI, '0x98dd4371579d35883BF37c84666b0300Ea619fFa').methods.balanceOf(address).call();
-    const mintedsoFar;
+    let mintedsoFar;
      await new  contractProvider.eth.Contract(minterABI, '0x0594FEe490F57f4eD3BDDDA0C3372480Aea6aD96').methods.minted.call(function(err, res){
       mintedsoFar = res;
     });
